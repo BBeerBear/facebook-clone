eb224f24516356b22219b8c401572f595837764d
Limin <xiongx659@gmail.com>
2023-01-15 13:20:58 -0500

    add mongodb mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const { readdirSync } = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 
+const { readdirSync } = require('fs');
 const app = express();
 app.use(cors());
 
 readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)));
 
-app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}..`);
 });
