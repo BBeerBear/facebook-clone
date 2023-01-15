b166004c90481d41753fe1e6ec097741fa3e59fd
Limin <xiongx659@gmail.com>
2023-01-15 12:50:56 -0500

    add controller

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const { readdirSync } = require('fs');
+
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('welcome from home');
-});
-app.get('/books', (req, res) => {
-  res.send('welcome from books');
-});
+readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)));
+
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
 });
